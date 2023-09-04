61a6b2cfd37dce0e8c58297d3b73079596958f64
Cristian <cristian84molina@gmail.com>
2023-09-04 20:00:36 -0300

    Data.js

--- Crossing-Borders/Data.js
+++ Crossing-Borders/Data.js
@@ -1,18 +1,5 @@
 export const camisetas = [
-    {
-        Club: "PSG Paris Saint-Germain",
-        imagenes: [
-        "https://i.postimg.cc/X7xPK91f/Tailandia-Camiseta-Paris-Saint-Germain-Special-23-24-image-155x155-removebg-preview.png",
-          "https://i.postimg.cc/3RgSkGDD/camisa-concept-psg-paris-saint-germain-2023-modelo-fan-torcedor-masculina-colorida-neymar-ney-njr-li.png",
-          "https://i.postimg.cc/d1MNQ6DY/camisa-concept-psg-paris-saint-germain-2023-removebg-preview.png",
-          "https://i.postimg.cc/T3w4thBZ/descarga-removebg-preview.png",
-          "https://i.postimg.cc/K8Hswx24/images-1-removebg-preview.png",
-        ],
-        precio: 22,
-        codigo: "PSG-001",
-        detalle: "Camiseta Concept Paris Saint-Germain 2023.",
-      },
-      {
+     {
         Club: "PSG Paris Saint-Germain",
         imagenes: [
             "https://i.postimg.cc/K8Hswx24/images-1-removebg-preview.png",
@@ -23,7 +10,57 @@ export const camisetas = [
         ],
         precio: 22,
         codigo: "PSG-001",
-        detalle: "Camiseta Concept Paris Saint-Germain 2023.",
+        detalle: "Camiseta Concept Paris Saint-Germain 2023/2024.",
+      },
+      {
+        Club: "FC Barcelona",
+        imagenes: [
+          "https://i.postimg.cc/258hpmys/Camiseta-FC-Barcelona-Pre-Match-2023-2024-Baratas-camisetafutboles-2-removebg-preview.png",
+          "https://i.postimg.cc/GhGkGq65/camiseta-nike-fc-barcelona-pre-match-2023-2024-white-noble-red-university-gold-0-removebg-preview.png",
+          "https://i.postimg.cc/Z5LpD0bP/camiseta-fc-barcelona-entrenamiento-2024-removebg-preview.png",
+          "https://i.postimg.cc/P5H141jr/camiseta-nike-fc-barcelona-pre-match-2023-2024-white-noble-red-university-gold-2-removebg-preview.png",
+          
+        ],
+        precio: 22,
+        codigo: "BAR-001",
+        detalle: "Camiseta Entrenamiento FC Barcelona 2023/2024."
+      },
+      {
+        Club: "AFC Ajax",
+        imagenes: [
+            "https://i.postimg.cc/jSwPRKrm/0e1f5b7dadcc0c07701bd65ec0832746bd2900ec-removebg-preview.png",
+          "https://i.postimg.cc/6pkR3SW3/70ade158ef34098672a4c2e3cfc1369ec2e3e0d0-removebg-preview.png",
+          "https://i.postimg.cc/1Xvw33Xh/descarga-1-removebg-preview.png",
+          "https://i.postimg.cc/4NFpCyjp/dfbc1e3d19db822d19c751dc27dc2425cacc7700-removebg-preview.png"
+        ],
+        precio: 25,
+        codigo: "AJX-001",
+        detalle: "Camiseta Concept AFC Ajax 2023/2024."
+      },
+      {
+        Club: "Inter Miami CF",
+        imagenes: [
+          "https://i.postimg.cc/zGvYdzgL/sg-11134201-7qvdi-lhn49o3syq7d8e-tn.jpg",
+          "https://i.postimg.cc/8CrQSZwN/mls-jerseys-2023-adidas-detalles-inter-miami-zgh.jpg",
+          "https://i.postimg.cc/htYW6kL2/Sin-titrw45y6ulo-1-removebg-preview.png"
+        ],
+        precio: 20,
+        codigo: "MIA-001",
+        detalle: "Camiseta Visitante Inter Miami CF 2023/2024."
+      },
+      {
+        Club: "FC Barcelona",
+        imagenes: [
+            "https://i.postimg.cc/7h00h6ZP/FC-Barcelona-adulto-removebg.png",
+          "https://i.postimg.cc/cHsTC1wx/mini-felix-kv-web.webp",
+          "https://i.postimg.cc/jq3vqb5T/FC-Barcelona-adulto-removebg-preview.png"
+        ],
+        precio: 22,
+        codigo: "BAR-001",
+        detalle: "Camiseta Local FC Barcelona 2023/2024."
       }
+      
+      
+      
 
 ]
