d6878db5ad67d5d515469f084a711798c168fa25
Cristian <cristian84molina@gmail.com>
2023-09-10 18:02:33 -0300

    Seccions

--- Crossing-Borders/src/views/home/Home.jsx
+++ Crossing-Borders/src/views/home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../components/navBar/NavBar.jsx"
 import Carrusel from "../../components/carrusel/Carrusel.jsx";
 import style from './Home.module.css'
 import Card from "../../components/card/Card.jsx";
+import Seccion from "../seccion/Seccion.jsx";
 import { camisetas } from "../../../Data.js";
 
 function Home() {
@@ -10,6 +11,7 @@ function Home() {
       <div className={style.pageContainer} >
         <Navbar />
         <Carrusel/>
+        <Seccion />
         <div className={style.cardContainer}>
       {camisetas.map((camiseta, index) => (
         <Card key={index} camiseta={camiseta} />
---
+++ Crossing-Borders/src/views/seccion/Seccion.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import style from "./Seccion.module.css"
+
+function Seccion() {
+    return (
+        <div className={style.container}>
+          <div className={style.card}>
+            <img
+              className={style.image}
+              src="https://i.postimg.cc/QMqdcKzF/mapa-grande-satelite-detallado-de-europa-en-la-noche.jpg"
+              alt="Ligas Europeas"
+            />
+            <div className={style.overlay}>
+              <h2 className={style.title}>Ligas Europeas</h2>
+              <a href="/ligas-europeas" className={style.link}>
+                Ver más
+              </a>
+            </div>
+          </div>
+    
+          <div className={style.card}>
+            <img
+              className={style.image}
+              src="https://i.postimg.cc/qvyRM5XX/Bkup-An0-CYAE4gp-W.jpg"
+              alt="Ligas de América"
+            />
+            <div className={style.overlay}>
+              <h2 className={style.title}>Ligas de América</h2>
+              <a href="/ligas-america" className={style.link}>
+                Ver más
+              </a>
+            </div>
+          </div>
+    
+          <div className={style.card}>
+            <img
+              className={style.image}
+              src="https://i.postimg.cc/PqHqLQSG/Night-Earth.jpg"
+              alt="Mapa Mundi"
+            />
+            <div className={style.overlay}>
+              <h2 className={style.title}>Mundial</h2>
+              <a href="/mapa-mundi" className={style.link}>
+                Ver más
+              </a>
+            </div>
+          </div>
+        </div>
+      );
+  }
+
+export default Seccion;
